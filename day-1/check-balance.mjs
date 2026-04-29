bd8a1ba6b8e633dd52bffc429b36494435515daa
Naman Dubey <namandubey10@gmail.com>
2026-04-29 12:35:10 +0530

    Day 3 - Installed Solana CLI and generated a new key pair for it, then airdropped 1 SOL and checked the balance which is exactly 1 SOL

--- day-1/check-balance.mjs
+++ day-1/check-balance.mjs
@@ -7,6 +7,7 @@ import {
 const rpc = createSolanaRpc(devnet("https://api.devnet.solana.com"));
 
 const { value: balance } = await rpc.getBalance(address("EpaJqwsFVpTQMdf6XGmusDHUUyQ9d5dEZ36dJYbf9VbV")).send();
+console.log(`Balance: ${balance} lamports`);
 
 //Converting balance in lamports to balance in SOL by dividing it with 1_000_000_000
 const balanceInSol = Number(balance) / 1_000_000_000;
